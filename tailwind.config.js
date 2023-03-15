2c2e6f4df55f1973efbc475cfa429c50bac4d4b3
aallnneess <73443724+aallnneess@users.noreply.github.com>
2023-03-15 10:13:44 +0100

    first working step

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    colors: {
+      Strong_cyan: 'hsl(172, 67%, 45%)',
+      Very_dark_cyan: 'hsl(183, 100%, 15%)',
+      Dark_grayish_cyan: 'hsl(186, 14%, 43%)',
+      Grayish_cyan: 'hsl(184, 14%, 56%)',
+      Light_grayish_cyan: 'hsl(185, 41%, 84%)',
+      Very_light_grayish_cyan: 'hsl(189, 41%, 97%)',
+      White: 'hsl(0, 0%, 100%)'
+    },
+    fontFamily: {
+      'sans': ['"Space Mono"', 'monospace']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
